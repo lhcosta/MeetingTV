0f52822312323f4091643854976faa6cd5e5b323
Caio Azevedo <caioba19@gmail.com>
2019-11-28 15:14:06 -0300

    Atualização do tempo da labelTimer

--- MeetingTV/MeetingTV/CollectionViews/MeetingViewController.swift
+++ MeetingTV/MeetingTV/CollectionViews/MeetingViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import CloudKit
 
-class MeetingViewController: UIViewController {
+class MeetingViewController: UIViewController, UpdateTimerDelegate {
     
+    @IBOutlet weak var labelTimer: UILabel!
     @IBOutlet var topicsCollectionView: UICollectionView!
     var meeting: Meeting!
     var topics: [Topic]!
@@ -19,21 +20,28 @@ class MeetingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let chronometer = Chronometer(delegate: self)
+        chronometer?.config()
+        
 //        let record = CKRecord()
 //        meeting = Meeting(record: record)
 //
 //        meeting.
     }
+    
+    func updateLabel(_ stringLabel: String!) {
+        labelTimer.text = stringLabel
+       }
 }
 
 
-extension MeetingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-}
+//extension MeetingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//}
